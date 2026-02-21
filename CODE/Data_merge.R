cbe98a6b893fdf9325f570cc0fa958c29b4ee730
Boris Gerát <geratb3@gmail.com>
2026-02-21 20:55:23 +0100

    Initial commit

--- CODE/Data_merge.R
+++ CODE/Data_merge.R
@@ -1,14 +1,1 @@
-set.seed(123)
-
-t <- 1:100
-y <- cumsum(rnorm(100))
- 
-plot(t, y,
-     type = "l",
-     lwd = 2,
-     col = "darkgreen",
-     main = "Random Walk",
-     xlab = "Time",
-     ylab = "Value")
-
-grid()
+print("hello world")
